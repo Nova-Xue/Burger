8add82a7cdd67a582e3f2f6e1c231b3cbd8162d0
Nova <xueshengyuan1118@hotmail.com>
2019-07-10 23:31:01 -0400

    Change database config from local database to jawsdb.

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ var mysql = require("mysql");
 require("dotenv").config();
 //create mysql connection
 var connection = mysql.createConnection({
-    host : process.env.DB_HOST,
+    host : process.env.DB_HOST ||"l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port : 3306,
-    user : process.env.DB_USER,
-    password : process.env.DB_PWD,
-    database : process.env.DB_DATABASE
+    user : process.env.DB_USER || "mgvjdqogkge527cg",
+    password : process.env.DB_PWD || "b3odfgpvu6g4w27p",
+    database : process.env.DB_DATABASE || "ooxwtpgzmiwufh96"
 });
 //connect to mysql db
 connection.connect(function(){
